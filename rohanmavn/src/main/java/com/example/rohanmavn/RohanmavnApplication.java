a2f74b3897106d036d1b278a027fd4a9e6d443e3
apmosys <apmosys@APMOSYSLT0171>
2022-07-26 17:24:49 +0530

    fdkf

---
+++ rohanmavn/src/main/java/com/example/rohanmavn/RohanmavnApplication.java
@@ -0,0 +1,16 @@
+package com.example.rohanmavn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RohanmavnApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(RohanmavnApplication.class, args);
+		
+		System.out.println("Hello");
+	}
+
+}
---
+++ rohanmavn/src/test/java/com/example/rohanmavn/RohanmavnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rohanmavn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RohanmavnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
